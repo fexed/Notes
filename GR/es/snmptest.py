a2aad5f762dfba040271925abf989b90b0f7a8ee
Federico Matteoni <fedex15@live.it>
2020-03-23 11:51:48 +0100

    update

--- GR/es/snmptest.py
+++ GR/es/snmptest.py
@@ -11,6 +11,7 @@
 
 # Imports
 import sys
+from time import sleep
 from pysnmp.hlapi import *
 
 # Params check
@@ -75,3 +76,28 @@ else:
     else:
         memTotal = varBinds[0].prettyPrint().split("=")[1].strip()
         print("Mem total =\t" + memTotal + " KB")
+
+# loop
+for i in range(5):
+    sleep(1)
+    print("\t\t######")
+    errInd, errName, errIndex, varBinds = next(getCmd(SnmpEngine(), CommunityData(community, mpModel=0), UdpTransportTarget((hostname, 161)), ContextData(), ObjectType(ObjectIdentity('UCD-SNMP-MIB', 'ssCpuIdle', 0))))
+
+    if errInd:
+        print(errInd)
+    elif errName:
+        print('Errore %s@[%s]' % (errName, varBinds[int(errIndex) - 1][0]))
+    else:
+        cpuIdle = varBinds[0].prettyPrint().split("=")[1].strip()
+        print("CPU =\t\t" + cpuIdle + "%")
+
+    errInd, errName, errIndex, varBinds = next(getCmd(SnmpEngine(), CommunityData(community, mpModel=0), UdpTransportTarget((hostname, 161)), ContextData(), ObjectType(ObjectIdentity('UCD-SNMP-MIB', 'memAvailReal', 0))))
+    
+    if errInd:
+        print(errInd)
+    elif errName:
+        print('Errore %s@[%s]' % (errName, varBinds[int(errIndex) - 1][0]))
+    else:
+        memAvail = varBinds[0].prettyPrint().split("=")[1].strip()
+        print("Mem =\t\t" + memAvail + "/" + memTotal + " KB")
+
