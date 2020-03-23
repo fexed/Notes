170d24d58531a29929b2ebb65da412a740530cf5
Federico Matteoni <fedex15@live.it>
2020-03-23 11:40:04 +0100

    update

--- GR/es/snmptest.py
+++ GR/es/snmptest.py
@@ -1,18 +1,28 @@
-#author: Federico Matteoni
-#date: 20/03/2020
-#tested-with: public demo.snmplabs.com
+#!/usr/bin/python3
 
+# Documentation
+# Author: Federico Matteoni
+# Date: 20/03/2020
+#
+# Installation
+# pip install pysnmp
+#
+# Example: python snmptest.py public demo.snmplabs.com
+
+# Imports
 import sys
 from pysnmp.hlapi import *
 
+# Params check
 if len(sys.argv) != 3:
     print("usage: " + sys.argv[0] + " <community> <hostname>")
 else:
     print("Requesting...")
     hostname = sys.argv[2]
     community = sys.argv[1]
-    
-    #ssCpuIdle    
+####
+
+# ssCpuIdle
     errInd, errName, errIndex, varBinds = next(getCmd(SnmpEngine(), CommunityData(community, mpModel=0), UdpTransportTarget((hostname, 161)), ContextData(), ObjectType(ObjectIdentity('UCD-SNMP-MIB', 'ssCpuIdle', 0))))
 
     if errInd:
@@ -23,7 +33,7 @@ else:
         cpuIdle = varBinds[0].prettyPrint().split("=")[1].strip()
         print("CPU =\t\t" + cpuIdle + "%")
     
-    #memAvailReal
+# memAvailReal
     errInd, errName, errIndex, varBinds = next(getCmd(SnmpEngine(), CommunityData(community, mpModel=0), UdpTransportTarget((hostname, 161)), ContextData(), ObjectType(ObjectIdentity('UCD-SNMP-MIB', 'memAvailReal', 0))))
     
     if errInd:
@@ -34,7 +44,7 @@ else:
         memAvail = varBinds[0].prettyPrint().split("=")[1].strip()
         print("Mem avail. =\t" + memAvail + " KB")
 
-    #memTotalReal
+# memTotalReal
     errInd, errName, errIndex, varBinds = next(getCmd(SnmpEngine(), CommunityData(community, mpModel=0), UdpTransportTarget((hostname, 161)), ContextData(), ObjectType(ObjectIdentity('UCD-SNMP-MIB', 'memTotalReal', 0))))
     
     if errInd:
