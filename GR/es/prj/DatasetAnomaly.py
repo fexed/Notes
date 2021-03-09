0310b213f4e9f5dd507ee1c00ecd2e894b0507c0
Federico Matteoni <fedex15@live.it>
2021-03-09 15:27:46 +0100

    Better anomalies Sounds stupid I know

--- GR/es/prj/DatasetAnomaly.py
+++ GR/es/prj/DatasetAnomaly.py
@@ -30,8 +30,25 @@ def createDataset():
 # Create a dataset for an anomalous day
 def createAnomalousDataset():
     dataset = []
-    for i in range(288): # 00 to 09
-        dataset.append(randint(0,400))
+    for i in range(108): # 00 to 09
+        dataset.append(randint(0,20))
+    for i in range(48): # 09 to 13 (Work)
+        elem = randint(100,350)
+        dataset.append(elem)
+    for i in range(12): # 13 to 14
+        elem = randint(0,20)
+        dataset.append(elem)
+    for i in range(48): # 14 to 18 (Work)
+        elem = randint(40,80)
+        dataset.append(elem)
+    for i in range(36): # 18 to 21
+        elem = randint(0,10)
+        dataset.append(elem)
+    for i in range(24): # 21 to 23 (It's time for Netflix)
+        elem = randint(100,350)
+        dataset.append(elem)
+    for i in range(12): # 23 to 24
+        dataset.append(randint(0,10))
     return dataset
 
 def dataToJson(dataset, filename):
