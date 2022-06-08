4df2e82aab029133bcd4520a060bc9d17767fc97
Federico Matteoni <fedex15@live.it>
2022-06-08 16:22:42 +0200

    Update converter.py

--- UniPi_CSMaster_AI/AlgorithmEngineering/code/converter.py
+++ UniPi_CSMaster_AI/AlgorithmEngineering/code/converter.py
@@ -1,4 +1,4 @@
-def converter(x, k):
+def converter(x : float, k : int):
     assert x >= 0
     assert x < 1
     out = "."
@@ -10,3 +10,22 @@ def converter(x, k):
             x -= 1
     
     return out
+
+
+def parse(x : str):
+    x = x[1:]
+    orig = 0.0
+    for i, d in enumerate(x):
+        orig += float(int(d)*(2**(-(i+1))))
+    return orig
+
+
+num = 0.53928457
+k = 1
+conv = converter(num, k)
+orig = parse(conv)
+err = num-orig
+print("converter(" + str(num) + ", " + str(k) + ") = " + conv)
+print("Parsed yields\t" + str(orig))
+print("Estimated error is 2^(-" + str(k) + ") = " + str(2**-k))
+print("Computed error = " + ("{:.10f}").format(err))
