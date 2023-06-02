1538b8ba0047add8296d96b12124559101955fd5
Federico Matteoni <fedex15@live.it>
2023-06-02 13:44:39 +0200

    Update huffman_pthread.cpp

--- UniPi_CSMaster_AI/ParallelandDistributedSystems/code/huffman/huffman_pthread.cpp
+++ UniPi_CSMaster_AI/ParallelandDistributedSystems/code/huffman/huffman_pthread.cpp
@@ -12,18 +12,18 @@ typedef struct {
     shared_ptr<string> text;
     vector<char> items;
     vector<int> frequencies;
-} FarmWorkerData;
+} PortionWorkerData;
 unsigned int currentStartingPosition = 0;
-pthread_mutex_t farmMutex;
+pthread_mutex_t portionMutex;
 
 void* computePortionData(void* param) {
-    FarmWorkerData* data = (FarmWorkerData*)param;
+    PortionWorkerData* data = (PortionWorkerData*)param;
     int startingPosition;
 
     while(1) {
-        pthread_mutex_lock(&farmMutex);
+        pthread_mutex_lock(&portionMutex);
         startingPosition = currentStartingPosition++;
-        pthread_mutex_unlock(&farmMutex);
+        pthread_mutex_unlock(&portionMutex);
 
         if (startingPosition*PORTION_SIZE >= data->text->length()) break;
         string localPortion = data->text->substr(startingPosition*PORTION_SIZE, PORTION_SIZE);
@@ -33,7 +33,29 @@ void* computePortionData(void* param) {
     pthread_exit(NULL);
 }
 
+typedef struct {
+    shared_ptr<MinHeap> minHeap;
+    shared_ptr<vector<char>> items;
+    shared_ptr<vector<int>> frequencies;
+} HeapWorkerData;
+unsigned int currentIndex = 0;
+pthread_mutex_t heapMutex;
 
+void* buildHeap(void* param) {
+    HeapWorkerData* data = (HeapWorkerData*)param;
+    int index;
+
+    while(1) {
+        pthread_mutex_lock(&heapMutex);
+        index = currentIndex++;
+        pthread_mutex_unlock(&heapMutex);
+
+        if(index >= data->items->size()) break;
+        data->minHeap->list.push_back(createNode(data->items->at(index), data->frequencies->at(index)));
+    }
+
+    pthread_exit(NULL);
+}
 
 int main(int argc, char **argv) {
     ARG_CHECK
@@ -46,20 +68,17 @@ int main(int argc, char **argv) {
     map<char, string> codes;
     int list[MAX_HEIGHT];
     int top = 0;
-    int size = 0;
     string encoded;
 
     {
         utimer tthreads("Huffman codes pthread");
         pthread_t threads[MAX_THREADS];
-        FarmWorkerData data[MAX_THREADS];
-        int numPortions = text->length() / PORTION_SIZE;
-        int start = 0;
+        PortionWorkerData portionData[MAX_THREADS];
 
         for (int i = 0; i < MAX_THREADS; i++) {
-            data[i].text = text;
+            portionData[i].text = text;
 
-            if (pthread_create(&threads[i], NULL, computePortionData, (void*)&data[i]) != 0) {
+            if (pthread_create(&threads[i], NULL, computePortionData, (void*)&portionData[i]) != 0) {
                 cout << "Error creating thread" << endl;
                 return -3;
             }
@@ -70,17 +89,41 @@ int main(int argc, char **argv) {
                 cout << "Error joining thread" << endl;
                 return -4;
             } else {
-                for (int j = 0; j < data[i].items.size(); j++) {
-                    std::vector<char>::iterator itr = std::find(items.begin(), items.end(), data[i].items[j]);
+                for (int j = 0; j < portionData[i].items.size(); j++) {
+                    std::vector<char>::iterator itr = std::find(items.begin(), items.end(), portionData[i].items[j]);
                     if (itr != items.cend()) {
-                        frequencies[std::distance(items.begin(), itr)] += data[i].frequencies[j];
+                        frequencies[std::distance(items.begin(), itr)] += portionData[i].frequencies[j];
                     } else {
-                        items.push_back(data[i].items[j]);
-                        frequencies.push_back(data[i].frequencies[j]);
+                        items.push_back(portionData[i].items[j]);
+                        frequencies.push_back(portionData[i].frequencies[j]);
                     }
                 }
             }
         }
+
+        auto minHeap = createMinimumHeap(items.size());
+        HeapWorkerData heapData[MAX_THREADS];
+        for (int i = 0; i < MAX_THREADS; i++) {
+            heapData[i].minHeap = minHeap;
+            heapData[i].items = make_shared<vector<char>>(items);
+            heapData[i].frequencies = make_shared<vector<int>>(frequencies);
+
+            if (pthread_create(&threads[i], NULL, buildHeap, (void*)&heapData[i]) != 0) {
+                cout << "Error creating thread" << endl;
+                return -3;
+            }
+        }
+
+        for (int i = 0; i < MAX_THREADS; i++) {
+            if (pthread_join(threads[i], NULL) != 0) {
+                cout << "Error joining thread" << endl;
+                return -4;
+            }
+        }
+
+        minHeap->size = items.size();
+        buildMinimumHeap(minHeap);
+        
 
 //        readFileData(text, items, frequencies);
 
