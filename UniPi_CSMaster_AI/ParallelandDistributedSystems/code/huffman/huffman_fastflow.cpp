ae2a743762495f961327a27bf60416f8f0f04e29
Federico Matteoni <fedex15@live.it>
2023-06-11 16:26:09 +0200

    Update huffman_fastflow.cpp

--- UniPi_CSMaster_AI/ParallelandDistributedSystems/code/huffman/huffman_fastflow.cpp
+++ UniPi_CSMaster_AI/ParallelandDistributedSystems/code/huffman/huffman_fastflow.cpp
@@ -56,7 +56,9 @@ struct FarmWorker: ff_node_t<string, PortionWorkerData> {
 struct FarmCollector: ff_node_t<PortionWorkerData, PortionWorkerData> {
     PortionWorkerData* data;
 
-    FarmCollector(PortionWorkerData* portionWorkerData) : data(portionWorkerData) {}
+    FarmCollector() {
+        data = new PortionWorkerData();
+    }
 
     PortionWorkerData* svc(PortionWorkerData* portionData) {
         for (unsigned int j = 0; j < portionData->items.size(); j++) {
@@ -72,44 +74,73 @@ struct FarmCollector: ff_node_t<PortionWorkerData, PortionWorkerData> {
 
         return GO_ON;
     }
+    
+    void eosnotify(ssize_t id) {
+        ff_send_out(data);
+    }
 };
 
-struct HeapIndexer: ff_node_t<int> {
-    int max;
 
-    HeapIndexer(int max) : max(max) {}
+struct CodesBuilder: ff_node_t<PortionWorkerData, map<char, string>> {
+    map<char, string>* codes;
 
-    int* svc(int* ignored) {
-        for (int i = 0; i < max; i++) {
-            ff_send_out(new int(i));
+    CodesBuilder(map<char, string>* codes) : codes(codes) {}
+
+    map<char, string>* svc(PortionWorkerData* data) {
+        int list[MAX_HEIGHT];
+        int top = 0;
+        auto minHeap = createMinimumHeap(data->items.size());
+        for (unsigned int i = 0; i < data->items.size(); i++) {
+            minHeap->list.push_back(createNode(data->items[i], data->frequencies[i]));
         }
+        minHeap->size = data->items.size();
+        buildMinimumHeap(minHeap);
+        while (minHeap->size != 1) {
+            auto left = extract(minHeap);
+            auto right = extract(minHeap);
 
+            auto top = createNode('$', left->frequency + right->frequency);
+
+            top->left = left;
+            top->right = right;
+
+            insert(minHeap, top);
+        }
+        auto root = extract(minHeap);
+        generateCodes(root, list, top, *codes);
+        return codes;
+    }
+};
+
+struct EncoderData {
+    string portion;
+    map<char, string> codes;
+};
+struct EncoderPartitioner: ff_node_t<map<char, string>, EncoderData> {
+    string text;
+
+    EncoderPartitioner(const string& param) : text(param) {}
+
+    EncoderData* svc(map<char, string>* codes) {
+        unsigned int pos = 0;
+
+        while(pos * PORTION_SIZE <= text.length()) {
+            string portion(text.substr(pos*PORTION_SIZE, PORTION_SIZE));
+            EncoderData* data = new EncoderData();
+            data->portion = portion;
+            data->codes = *codes;
+            ff_send_out(data);
+            pos++;
+        }
         return EOS;
     }
 };
 
-struct HeapWorker: ff_node_t<int> {
-    shared_ptr<MinHeap> minHeap;
-    PortionWorkerData* data;
-
-    HeapWorker(shared_ptr<MinHeap> minHeap, PortionWorkerData* portionWorkerData) : minHeap(minHeap), data(portionWorkerData) {}
-
-    int* svc(int* index) {
-        minHeap->list[*index] = createNode(data->items[*index], data->frequencies[*index]);
-        delete index;
-        return GO_ON;
-    }
-};
-
-struct PortionEncoder: ff_node_t<string> {
-    map<char, string> codes;
-
-    PortionEncoder(map<char, string> codes) : codes(codes) {}
-
-    string* svc(string* portion) {
+struct PortionEncoder: ff_node_t<EncoderData, string> {
+    string* svc(EncoderData* data) {
         string* encoded = new string("");
-        *encoded = encodeText(*portion, codes);
-        delete portion;
+        *encoded = encodeText(data->portion, data->codes);
+        delete data;
         return encoded;
     }
 };
@@ -141,8 +172,6 @@ int main(int argc, char **argv) {
     vector<char> items;
     vector<int> frequencies;
     map<char, string> codes;
-    int list[MAX_HEIGHT];
-    int top = 0;
     string encoded;
     string decoded;
 
@@ -155,64 +184,27 @@ int main(int argc, char **argv) {
             portionFarmWorkers.push_back(make_unique<FarmWorker>());
         }
         ff_Farm<PortionWorkerData> portionFarm(move(portionFarmWorkers));
-        PortionWorkerData* data = new PortionWorkerData();
-        unique_ptr<FarmCollector> collector = make_unique<FarmCollector>(data);
-
-        ff_Pipe<> dataPipeline(
-            inputStage,
-            portionFarm,
-            collector
-        );
-        dataPipeline.run_and_wait_end();
-        
-        auto minHeap = createMinimumHeap(data->items.size());
-        for (unsigned int i = 0; i < data->items.size(); i++) {
-            minHeap->list.push_back(nullptr);
-        }
-        
-        unique_ptr<HeapIndexer> heapIndexer = make_unique<HeapIndexer>(data->items.size());
-        vector<unique_ptr<ff_node>> heapFarmWorkers;
-        for (int i = 0; i < MAX_THREADS; i++) {
-            heapFarmWorkers.push_back(make_unique<HeapWorker>(minHeap, data));
-        }
-        ff_Farm<int> heapFarm(move(heapFarmWorkers));
-
-        ff_Pipe<> heapPipeline(
-            heapIndexer,
-            heapFarm
-        );
-        heapPipeline.run_and_wait_end();
-
-        minHeap->size = data->items.size();
-        buildMinimumHeap(minHeap);
-        while (minHeap->size != 1) {
-            auto left = extract(minHeap);
-            auto right = extract(minHeap);
-
-            auto top = createNode('$', left->frequency + right->frequency);
-
-            top->left = left;
-            top->right = right;
-
-            insert(minHeap, top);
-        }
-        auto root = extract(minHeap);
-        generateCodes(root, list, top, codes);
-
-        unique_ptr<Partitioner> partitioner = make_unique<Partitioner>(*text);
+        unique_ptr<FarmCollector> collector = make_unique<FarmCollector>();
+        unique_ptr<CodesBuilder> coder = make_unique<CodesBuilder>(&codes);
+        unique_ptr<EncoderPartitioner> partitioner = make_unique<EncoderPartitioner>(*text);
         vector<unique_ptr<ff_node>> encoders;
         for (int i = 0; i < MAX_THREADS; i++) {
-            encoders.push_back(make_unique<PortionEncoder>(codes));
+            encoders.push_back(make_unique<PortionEncoder>());
         }
         ff_OFarm<PortionEncoder> encoderFarm(move(encoders));
         unique_ptr<Concat> concat = make_unique<Concat>(&encoded);
         
-        ff_Pipe<> encoderPipeline(
+
+        ff_Pipe<> mainPipeline(
+            inputStage,
+            portionFarm,
+            collector,
+            coder,
             partitioner,
             encoderFarm,
             concat
         );
-        encoderPipeline.run_and_wait_end();
+        mainPipeline.run_and_wait_end();
     }
 
     if (VERIFY) {   
